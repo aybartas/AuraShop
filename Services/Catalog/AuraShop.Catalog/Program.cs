d216238acae602cdc8fcd89787cf258015a82f88
aybar <aybartass@gmail.com>
2025-06-09 03:34:20 +0300

    yarp reverse proxy added

--- Services/Catalog/AuraShop.Catalog/Program.cs
+++ Services/Catalog/AuraShop.Catalog/Program.cs
@@ -37,6 +37,8 @@ BsonSerializer.RegisterSerializer(typeof(Guid), new GuidSerializer(GuidRepresent
 
 builder.Services.AddSingleton<SeedService>();
 
+builder.Services.AddAuthorization();
+
 builder.Services.AddCommonServices(typeof(CatalogAssembly));
 
 var app = builder.Build();
